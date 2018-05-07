ba776908a8fff917841af234aafea7249812f6df
Raul <raulrbander@gmail.com>
2018-05-07 10:57:30 -0400

    Comentario multilinea

--- scanner.cpp
+++ scanner.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
 			posicion = 1;
 		}
 
-		if (ntoken == LSTRING || ntoken == IDENTIFIER) {
+		if (ntoken == STRING || ntoken == IDENTIFIER) {
 			// Crea token identificador y coloca en el vector.
 			tokens.push_back(new TokenIdentificador(ntoken,yylineno,posicion,yytext));
 		}
