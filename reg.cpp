f0141a89f69751ff2ea50b2254bace666d813ab2
Raul <raulrbander@gmail.com>
2018-11-28 03:43:34 -0400

    Inicio de algoritmo de variables vivas y algoritmo que asigna aristas al grafo

---
+++ reg.cpp
@@ -0,0 +1,4 @@
+//Funcion que determina el numero de regitros
+
+m = [["a","b","r"],["r","t1","c"],["a","c","t2"],["t2","b","d"],["a","d","m"],["c","b","n"],["m","b","a"],["p","m","t3"],["t3","t4","f"],["f","g","h"]]
+
